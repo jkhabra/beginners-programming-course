0ce3660bf4ece549a94e843d5abac37af740803e
Jatinder Singh <jkhabra09@gmail.com>
2017-01-29 10:35:08 +0530

    Add code of Merge Sort

--- code/algorithms.py
+++ code/algorithms.py
@@ -1,5 +1,4 @@
 def selection_sort(ls):
-
     if len(ls) <= 1:
         return ls
 
@@ -13,9 +12,43 @@ def bubble_sort(ls):
     n = len(ls)
 
     for i in range(n - 1):
-
         for i in range(n - 1):
-
             if ls[i] > ls[i + 1]:
                 ls[i], ls[i+1] = ls[i+1], ls[i]
     return ls
+
+
+def merge(left, right):
+    """Join sorted lists left and right and returns new sorted list"""
+    result = []
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+
+    return result
+
+
+def merge_sort(ls):
+    if len(ls) < 2:
+        return ls
+
+    mid = len(ls) // 2
+    left = merge_sort(ls[:mid])
+    right = merge_sort(ls[mid:])
+
+    return merge(left, right)
--- code/test_algorithms.py
+++ code/test_algorithms.py
@@ -1,4 +1,4 @@
-from algorithms import selection_sort, bubble_sort
+from algorithms import selection_sort, bubble_sort, merge_sort
 
 ls1 = [2, 5, 6, 7, 8, 1]
 ls2 = [1]
@@ -21,3 +21,11 @@ def test_bubble_sort():
     assert bubble_sort(ls3) == sorted(ls3)
     assert bubble_sort(ls4) == sorted(ls4)
     assert bubble_sort(ls5) == sorted(ls5)
+
+
+def test_merge_sort():
+    assert merge_sort(ls1) == sorted(ls1)
+    assert merge_sort(ls2) == sorted(ls2)
+    assert merge_sort(ls3) == sorted(ls3)
+    assert merge_sort(ls4) == sorted(ls4)
+    assert merge_sort(ls5) == sorted(ls5)
